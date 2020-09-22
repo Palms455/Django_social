262da7b660068ca79b6b04108630c8d3c4107040
Рустам Каюмов <rustrnb@yandex.ru>
2020-09-23 00:07:27 +0500

    Регистрация пользователя

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', auth.LoginView.as_view(), name='login'),
+    path('logout/', auth.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('password_change/', auth.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('register/', views.register, name='register')
 ]
+'''
+вместо прописывания стандартных путей можно ввести 
+path('', include('django.contrib.auth.urls')
+'''
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 """
@@ -14,6 +14,18 @@ PasswordResetDoneView - перенаправл после смены парол�
 PasswordResetConfirmView - указ новый пароль
 PasswordResetCompView - сообщение об успешной смене пароля 
 """
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            # set_password зашифровывает пароль
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
 
 def user_login(request):
     if request.method == 'POST':
