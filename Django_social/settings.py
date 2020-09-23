b142baf331e273e5874b64b38ec772f2c43b8ffb
Рустам Каюмов <rustrnb@yandex.ru>
2020-09-23 23:47:03 +0500

    Расширение модели User

--- Django_social/settings.py
+++ Django_social/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from .settings_local import DEBUG, DATABASES
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, verbose_name="Фотография")
+
+    def __str__(self):
+        return f"Профиль пользователя {self.user.username}"
