98545c99dcf981fe19b0ab6f02b6a1df16b84b30
Рустам Каюмов <rustrnb@yandex.ru>
2020-09-27 07:31:57 +0500

    Custom decorator and ajax like

--- images/views.py
+++ images/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
+from common.decorators import ajax_required
 
 
 @login_required
@@ -39,3 +40,21 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image})
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': ok})
+        except:
+            pass
+    return JsonResponse({'status': ok})
