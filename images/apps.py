5e1d09d220ea5577c9eee432419e1c0d7c922075
Рустам Каюмов <rustrnb@yandex.ru>
2020-10-04 08:08:00 +0500

    Использование signals

--- images/apps.py
+++ images/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     name = 'images'
+
+    def ready(self):
+        # для отслеживания изменений импортируем файл с функцией-подписчиком на сигнал
+        from . import signals
